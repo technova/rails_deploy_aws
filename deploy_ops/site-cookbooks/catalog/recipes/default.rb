0b05316d52cfecd6742b0db69256f430acacab65
Rajesh Soni <rajesh.soni@saviance.com>
2014-05-24 02:39:00 +0530

    added simple recipe

---
+++ deploy_ops/site-cookbooks/catalog/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: catalog
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+execute "simple command" do
+  action :run
+  command "cd /srv/www/goldappnew/current && touch log/m.txt"
+end
